c278027945234a17decf86159cdafcab1e64944a
guimaraes mahota <guimaraesmahota@gmail.com>
2015-05-27 16:46:07 +0200

    update do extrato dos clientes

--- CSU_CRM_WEB/CSU_CRM_WEB/Content/Reports/Extrato.cs
+++ CSU_CRM_WEB/CSU_CRM_WEB/Content/Reports/Extrato.cs
@@ -16,14 +16,14 @@ namespace CSU_CRM_WEB.Content.Reports {
     using CrystalDecisions.CrystalReports.Engine;
     
     
-    public class Clientes : ReportClass {
+    public class Extrato : ReportClass {
         
-        public Clientes() {
+        public Extrato() {
         }
         
         public override string ResourceName {
             get {
-                return "Clientes.rpt";
+                return "Extrato.rpt";
             }
             set {
                 // Do nothing
@@ -41,7 +41,7 @@ namespace CSU_CRM_WEB.Content.Reports {
         
         public override string FullResourceName {
             get {
-                return "CSU_CRM_WEB.Content.Reports.Clientes.rpt";
+                return "CSU_CRM_WEB.Content.Reports.Extrato.rpt";
             }
             set {
                 // Do nothing
@@ -66,7 +66,7 @@ namespace CSU_CRM_WEB.Content.Reports {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section3 {
+        public CrystalDecisions.CrystalReports.Engine.Section GroupHeaderSection1 {
             get {
                 return this.ReportDefinition.Sections[2];
             }
@@ -74,7 +74,7 @@ namespace CSU_CRM_WEB.Content.Reports {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section4 {
+        public CrystalDecisions.CrystalReports.Engine.Section Section3 {
             get {
                 return this.ReportDefinition.Sections[3];
             }
@@ -82,7 +82,7 @@ namespace CSU_CRM_WEB.Content.Reports {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section5 {
+        public CrystalDecisions.CrystalReports.Engine.Section GroupFooterSection1 {
             get {
                 return this.ReportDefinition.Sections[4];
             }
@@ -90,17 +90,25 @@ namespace CSU_CRM_WEB.Content.Reports {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.Shared.IParameterField Parameter_teste {
+        public CrystalDecisions.CrystalReports.Engine.Section Section4 {
             get {
-                return this.DataDefinition.ParameterFields[0];
+                return this.ReportDefinition.Sections[5];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.CrystalReports.Engine.Section Section5 {
+            get {
+                return this.ReportDefinition.Sections[6];
             }
         }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
-    public class CachedClientes : Component, ICachedReport {
+    public class CachedExtrato : Component, ICachedReport {
         
-        public CachedClientes() {
+        public CachedExtrato() {
         }
         
         [Browsable(false)]
@@ -137,7 +145,7 @@ namespace CSU_CRM_WEB.Content.Reports {
         }
         
         public virtual CrystalDecisions.CrystalReports.Engine.ReportDocument CreateReport() {
-            Clientes rpt = new Clientes();
+            Extrato rpt = new Extrato();
             rpt.Site = this.Site;
             return rpt;
         }
